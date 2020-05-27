9947746f765dbe03c1dbb862b7e77f6663e02f0d
Yuliya Payevska <y.payevska@gmail.com>
2020-05-27 14:13:14 +0300

    finished 39 lesson

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
+import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/state';
+
+/* let addPostActionCreator = () => {
+  return {
+    type: 'ADD-POST'
+  }
+}
+
+let updateNewPostTextActionCreator = (text) => {
+  return {
+    type: 'UPDATE-NEW-POST-TEXT', newText: text
+  }
+} */
 
 function MyPosts(props) {
 
@@ -9,12 +22,13 @@ function MyPosts(props) {
   let newPostElement = React.createRef();
 
   let addPost = () => { 
-    props.dispatch({type: 'ADD-POST'});
+    props.dispatch(addPostActionCreator());
   };
 
   let onPostChange = () => {
     let text = newPostElement.current.value;
-    let action = {type: 'UPDATE-NEW-POST-TEXT', newText: text};
+    /* let action = {type: 'UPDATE-NEW-POST-TEXT', newText: text}; */
+    let action = updateNewPostTextActionCreator(text);
     props.dispatch(action);
   }
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
 let store = {
   _state: {
     profilePage: {
@@ -51,5 +54,11 @@ let store = {
     }
   }
 }
+
+export const addPostActionCreator = () => 
+  ({type: ADD_POST});
+
+export const updateNewPostTextActionCreator = (text) => 
+({type: UPDATE_NEW_POST_TEXT, newText: text});
 
 export default store;
