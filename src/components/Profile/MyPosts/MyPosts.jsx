2a61b59d43d5fe9cf2d3d07dcf9b9957d993111b
Yuliya Payevska <y.payevska@gmail.com>
2020-05-28 17:03:33 +0300

    43 lesson

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
-import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/profile-reducer';
-
 
 function MyPosts(props) {
 
@@ -10,14 +8,13 @@ function MyPosts(props) {
   
   let newPostElement = React.createRef();
 
-  let addPost = () => { 
-    props.dispatch(addPostActionCreator());
+  let onAddPost = () => { 
+    props.addPost();
   };
 
   let onPostChange = () => {
     let text = newPostElement.current.value;
-    let action = updateNewPostTextActionCreator(text);
-    props.dispatch(action);
+    props.updateNewPostText(text);
   }
 
   return (        
@@ -28,7 +25,7 @@ function MyPosts(props) {
           <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText}/>
         </div>
         <div>
-          <button onClick={addPost}>Add post</button>
+          <button onClick={onAddPost}>Add post</button>
         </div>
       </div>
       <div className={s.posts}>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import profileReducer from "./profile-reducer";
+/* import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
 
@@ -49,4 +49,4 @@ let store = {
   }
 }
 
-export default store;
+export default store; */
